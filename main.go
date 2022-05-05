f049c6a0d65bbe860499384b9b763c9950370d31
SangMin <sangmin4208@gmail.com>
2022-05-05 22:59:43 +0900

    You're doing great ✨

--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+type contactInfo struct {
+	email   string
+	zipCode int
+}
 
 func main() {
-	fmt.Println("Hello World")
+	alex := person{
+		firstName: "Alex",
+		lastName:  "Anderson",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+	// var alex person
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
+	// alex.zipCode = 94000
+	// alex.email = "jim@gmail.com"
+	alex.updateName("Jimmi")
+	alex.print()
+	(&alex).print()
+}
+
+func (p *person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
